a58c6f75bec1a6d8492d47ac1e40af1f1114d130
Martina_Harsanikova <martinaharsanikova@gmail.com>
2024-01-28 17:49:17 +0100

    Ukol_cast_1

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -11,8 +11,45 @@ public class HlavniProgram {
 
   public void start() {
     zofka = new Turtle();
-
     //TODO implementace domácího úkolu
+    nakresliPrasatko();
   }
 
+  public void nakresliTelo() {
+    for (int i=0; i<2; i++) {
+      zofka.move(250);
+      zofka.turnRight(90);
+      zofka.move(200);
+      zofka.turnRight(90);
+    }
+  }
+  public void hlava() {
+    zofka.move(Math.sqrt(2) * 100);
+    zofka.turnRight(90);
+    zofka.move(Math.sqrt(2) * 100);
+    zofka.turnRight(45);
+  }
+  public void nohy() {
+    zofka.turnRight(180);
+    zofka.move(50);
+    zofka.turnRight(90);
+    zofka.move(50);
+  }
+  public void nakresliPrasatko() {
+    zofka.turnLeft(45);
+    hlava();
+    nakresliTelo();
+    zofka.turnRight(90);
+    zofka.move(200);
+    zofka.turnRight(45);
+    zofka.move(50);
+    nohy();
+    zofka.turnRight(180);
+    zofka.move(50);
+    zofka.turnRight(135);
+    zofka.move(250);
+    zofka.turnRight(135);
+    zofka.move(50);
+    nohy();
+  }
 }
